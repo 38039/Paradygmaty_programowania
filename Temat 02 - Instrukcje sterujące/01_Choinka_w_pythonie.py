56bfbcf72cb26d973a72ee46cd25ac223b0f6d88
38039 <38039@student.atar.edu.pl>
2025-01-29 06:29:11 +0100

    Dodano TEMAT 2

---
+++ Temat 02 - Instrukcje sterujące/01_Choinka_w_pythonie.py
@@ -0,0 +1,35 @@
+# TEMAT   1:  INSTRUKCJE STERUJĄCE
+# ZADANIE 2:  CHOINKA W PYTHONIE
+#
+# WYMAGANIA:
+# Maksymalna liczba plików: 1
+# Rodzaj pracy:  Praca indywidualna
+#
+# TREŚĆ:
+# Napisz program w Pythonie rysujący "choinkę"
+#
+# Czyli funkcja ma mieć 3 parametry - dwie liczby całkowite
+# Liczba poziomów powinna być pobierana z klawiatury.
+# Choinka o parzystej liczbie poziomów powinna zostać "wyrysowana" gwiazdkami, a o nieparzystej - hashami (#).
+# Po wyrysowaniu choinki program ma ponownie pobrać liczbę poziomów i wyrysować nową choinkę.
+# Rysowanie choinek ma być powtarzane wielokrotnie (w pętli while) aż do wyrysowania choinki
+# o wysokości 7 (choinka o tej wysokości też ma zostać wyrysowana).
+# Rysowanie pojedynczej choinki powinno być zawarte w jednej pętli for
+# (z użyciem range oraz 'mnożenia liczby przez napis').
+
+h = 0
+while h < 7:
+    x = int(input("Podaj liczbe: "))
+    h = x
+    if x % 2 == 0:
+        tree = '*'
+    else:
+        tree = '#'
+
+    space = x
+    paint = 1
+    for i in range(x):
+        space = space - 1
+        print(space * " " + paint * tree)
+        paint = paint + 2
+    print()
---
+++ Temat 02 - Instrukcje sterujące/05_D_Monety.py
@@ -0,0 +1,10 @@
+# TEMAT   1:  INSTRUKCJE STERUJĄCE
+# ZADANIE 5:  MONETY (DODATKOWE)
+#
+# WYMAGANIA:
+# Maksymalna liczba plików: 1
+# Rodzaj pracy:  Praca indywidualna
+#
+# TREŚĆ:
+# Napisz program znajdujący wszystkie możliwości złożenia 100zł
+# z nominałów 2, 5 oraz 10 zł. Należy wypisać liczbę możliwych kombinacji.
