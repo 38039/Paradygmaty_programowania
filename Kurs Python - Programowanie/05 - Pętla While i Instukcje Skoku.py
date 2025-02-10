51b5b29941112d915f20d6c64dec000f6a292607
38039 <38039@student.atar.edu.pl>
2025-02-10 20:40:51 +0100

    Zawartość materiału piątego: while, continue, break

---
+++ Kurs Python - Programowanie/05 - Pętla While i Instukcje Skoku.py
@@ -0,0 +1,13 @@
+i = 0
+
+# PĘTLA WHILE
+while True:
+    i += 1
+
+    if i % 2 == 1:
+        continue    # PRZESKAKUJE DO POCZĄTKU PĘTLI
+
+    print(i)
+
+    if i > 10:
+        break       # ZABIJA PĘTLE (11 PODEJŚCIE)
